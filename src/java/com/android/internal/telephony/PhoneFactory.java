000739f0f12fced6f7b781e368e0bba402290ab5
jiachunxu <leo.jia@oneplus.net>
2016-12-22 10:33:42 +0800

    [OP5 specific] Add for community, but the community is not pleased
    Change-Id: I54dd9d0bffab8cecef92c3a4fbc36c9a8cef7b52

--- src/java/com/android/internal/telephony/PhoneFactory.java
+++ src/java/com/android/internal/telephony/PhoneFactory.java
@@ -531,4 +531,12 @@ public class PhoneFactory {
     public static SubscriptionInfoUpdater getSubscriptionInfoUpdater() {
         return sSubInfoRecordUpdater;
     }
+
+    //VENDOR_EDIT
+    /**@hide*/
+    public static PhoneSwitcher oemGetPhoneSwitcher()
+    {
+        return sPhoneSwitcher;
+    }
+    //end
 }
